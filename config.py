90bd1459feb6166ddce3f99c0fae6842026d15be
Alex <sasha.pankratiew@gmail.com>
2018-07-23 14:52:14 +0300

    Added config

---
+++ config.py
@@ -0,0 +1,8 @@
+login, password = 'login', 'password' # Логин и пароль аккаунта бота
+my_id = 111111 # ID бота (Пользователя)
+
+isUsedAntiCaptcha = True # Использовать антикапчу? 
+# Сервис: http://getcaptchasolution.com/qocusckanf
+
+antiCaptchaKey = ''
+# Получить ключ: https://anti-captcha.com/clients/settings/apisetup
